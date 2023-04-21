2054e86648821d588b6e4959fd9dcb7e54df3919
vanVanh <anhnv6083@gmail.com>
2023-04-21 22:50:31 +0700

    complete

--- controllers/site.controller.js
+++ controllers/site.controller.js
@@ -2,7 +2,13 @@ const models = require('../models/user.model');
 const fs = require('fs');
 
 exports.list = async (req, res, next) => {
-    let user = await models.Project.find().populate('work');
+
+    let search = null;
+    if(typeof req.query.status != 'undefined'){
+        search = {status: req.query.status};
+    }
+
+    let user = await models.Project.find(search).populate('id_work');
     res.render('index', { obj: user});
 }
 
@@ -10,7 +16,7 @@ exports.add = async (req, res, next) => {
     let work = await models.Work.find();
     if(req.method == 'POST'){
         let obj = new models.Project();
-        obj.work = req.body.work;
+        obj.id_work = req.body.work;
         obj.name = req.body.name;
         obj.description = req.body.description;
         obj.status = req.body.status;
@@ -36,11 +42,11 @@ exports.edit = async (req, res, next) => {
         obj.name = req.body.name;
         obj.description = req.body.description;
         obj.status = req.body.status;
-        fs.renameSync(req.file.path, './public/images/' + req.file.originalname);
-        obj.image = 'http://localhost:3000/images/' + req.file.originalname;
+        obj.image = req.body.image;
+        obj._id = id;
         try {
-            let data = await obj.save();
-            console.log(data);
+            await models.Project.findByIdAndUpdate({_id: id}, obj);
+            return res.redirect('/');
         } catch (error) {
             console.log(error.message);
         }
--- models/user.model.js
+++ models/user.model.js
@@ -1,21 +1,21 @@
 const db = require('./db');
 
+
+const projectSchema = new db.mongoose.Schema(
+    {
+        id_work: {type:db.mongoose.Schema.ObjectId, ref: 'Work'},
+        name: {type: String, required: true},
+        description: {type: String, required: true},
+        status: {type: String, required: false},
+        image: {type: String, required: false}
+    }
+)
 const workSchema = new db.mongoose.Schema(
     {
         name: {type: String, required: true}
     }
 )
 
-const projectSchema = new db.mongoose.Schema(
-    {
-        id: {type: String, required: true},
-        work: {type: db.mongoose.Schema.Types.ObjectId, ref: 'Work', required: true},
-        name: {type: String, required: true},
-        description: {type: String, required: true},
-        status: {type: String, required: true},
-        image: {type: String, required: true}
-    }
-)
 
 const Work = db.mongoose.model('Work', workSchema);
 const Project = db.mongoose.model('Project', projectSchema);
